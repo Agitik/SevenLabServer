2fa7e0bc89f25c97b60558f9871c52a6cbac3f0e
Agitik <62553989+Agitik@users.noreply.github.com>
2020-09-27 00:29:24 +0300

    Работает, оставляю.

--- src/NetworkExchange/Server.java
+++ src/NetworkExchange/Server.java
@@ -5,26 +5,25 @@ import DBWorker.db_config;
 import java.io.*;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
-import java.nio.channels.ServerSocketChannel;
-import java.nio.channels.SocketChannel;
+import java.nio.channels.*;
 import java.util.Iterator;
 import java.util.Scanner;
 import java.util.Set;
-
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 
 public class Server {
     public static boolean ExitCode = false;
-
+    public static ExecutorService sendForClient = Executors.newFixedThreadPool(5);
+    public static ExecutorService readClient = Executors.newCachedThreadPool();
+    public static ExecutorService exeClient = Executors.newCachedThreadPool();
     public static void server(int PORT) throws IOException, ClassNotFoundException {
         Selector selector = Selector.open();
         ServerSocketChannel serverSocket = ServerSocketChannel.open();
         serverSocket.bind(new InetSocketAddress("localhost", PORT));
         serverSocket.configureBlocking(false);
         serverSocket.register(selector, SelectionKey.OP_ACCEPT);
-        ByteBuffer buffer = ByteBuffer.allocate(1024 * 1024);
         Scanner scr = new Scanner(System.in);
         while (!ExitCode) {
             if (System.in.available() > 0) {
@@ -44,28 +43,50 @@ public class Server {
                     client.register(selector, SelectionKey.OP_READ);
                 }
                 if (key.isReadable() & !ExitCode) {
-                    SocketChannel client = (SocketChannel) key.channel();
-                    client.read(buffer);
-
-                    //Обработка вопроса
-                    ByteArrayInputStream bais = new ByteArrayInputStream(buffer.array());
-                    ObjectInputStream ois = new ObjectInputStream(bais);
-                    QuestionPack qp = (QuestionPack) ois.readObject();
-                    AnswerPack ap = UnPacker.UnPackAndExecute(qp);
-
-                    //Подготовка ответа
-                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                    ObjectOutputStream oos = new ObjectOutputStream(baos);
-                    oos.writeObject(ap);
-
-                    byte[] data = baos.toByteArray();
-
-                    client.write(ByteBuffer.wrap(data));
-                    buffer.clear();
+                    getClient(key.channel());
                     key.cancel();
                 }
                 iter.remove();
             }
         }
+    }
+
+    public static void getClient(Channel ch) throws IOException, ClassNotFoundException {
+        Runnable read = () -> {
+            SocketChannel client = (SocketChannel) ch;
+            ByteBuffer buffer = ByteBuffer.allocate(1024 * 1024);
+            try {
+                client.read(buffer);
+                //Обработка вопроса
+                ByteArrayInputStream bais = new ByteArrayInputStream(buffer.array());
+                ObjectInputStream ois = new ObjectInputStream(bais);
+                QuestionPack qp = (QuestionPack) ois.readObject();
+
+                Runnable exe = () -> {
+                    AnswerPack ap = UnPacker.UnPackAndExecute(qp);
+                    Runnable send = () -> {
+                        //Подготовка ответа
+                        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                        ObjectOutputStream oos = null;
+                        try {
+                            oos = new ObjectOutputStream(baos);
+                            oos.writeObject(ap);
+
+                            byte[] data = baos.toByteArray();
+
+                            client.write(ByteBuffer.wrap(data));
+                            buffer.clear();
+                        } catch (IOException e) {
+                            System.out.println("Чет не так с отправкой");
+                        }
+                    };
+                    sendForClient.submit(send);
+                };
+                exeClient.submit(exe);
+            } catch (IOException | ClassNotFoundException e) {
+                System.out.println("Косяк в чтении клиента");
+            }
+        };
+        readClient.submit(read);
     }
 }
